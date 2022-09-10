1cc2265e1d921bbb3a4470d0919994f96250997e
Brad Garropy <bgarropy@atlassian.com>
2022-09-10 12:42:14 -0500

    remove tests.

--- example/app/components/Footer/Footer.test.tsx
+++
@@ -1,9 +0,0 @@
-import {render, screen} from "@testing-library/react"
-import {expect, test} from "vitest"
-
-import Footer from "~/components/Footer"
-
-test("renders", () => {
-    render(<Footer />)
-    expect(screen.getByText("Footer"))
-})
