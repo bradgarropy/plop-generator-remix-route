41d792b39687430d557e4d0ad6259401c869079e
Brad Garropy <bgarropy@atlassian.com>
2022-09-11 21:56:17 -0500

    uses plop helper includes.

--- src/index.ts
+++ src/index.ts
@@ -2,17 +2,10 @@ import {NodePlopAPI} from "plop"
 
 import route from "./route"
 
-const generator = (plop: NodePlopAPI): void => {
+const generator = async (plop: NodePlopAPI): Promise<void> => {
+    await plop.load("@bradgarropy/plop-helper-includes")
+
     plop.setDefaultInclude({generators: true})
-
-    plop.setHelper("includes", (array, string, options) => {
-        if (array.includes(string)) {
-            return options.fn(this)
-        } else {
-            return options.inverse(this)
-        }
-    })
-
     plop.setGenerator("route", route)
 }
 
